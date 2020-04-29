687d8af5e751e5d0578595866b1a10a2175ca8d9
LucasGobbs <llcostagobbi@gmail.com>
2020-04-29 20:57:52 -0300

    Adicionando funçoes

--- example.c
+++ example.c
@@ -4,11 +4,15 @@
 #include "glalgebra.h"
 int main(){
   //teste(2);
-  Mat* matrix = Mat_create(10,10);
+  Mat* matrix = Mat_create(3,3);
  //Mat_get(0, 0);
   //teste(2);
   Mat_print(matrix);
   printf("%f",Mat_get(matrix, 2, 2));
+  matrix = Mat_set(matrix, 2, 2, 4.0);
+  printf("%f",Mat_get(matrix, 2, 2));
+    Mat_print(matrix);
+
   Mat_destroy(matrix);
   return 0;
 }
--- glalgebra.h
+++ glalgebra.h
@@ -18,33 +18,74 @@
 #define concat(p1, p2) _concat(p1, p2) 
 #define _concat(p1, p2) p1 ## p2       
 #define ns(iden) concat(NAMESPACE_PREFIX, iden)
-    
-#define TYPE float
-#define TYPE_DEBUG %f
+
+// type that the data in matrix is stored
+#ifndef TYPE
+    #define TYPE float
+
+    #define TYPE_DEBUG %f
+#endif //TYPE
+
+
 #include <stdio.h>
 #include <assert.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdint.h>
-/*================================== Types ==================================*/
+/*=================================================================================*/
+/*============================ Types ==================================*/
+/*=================================================================================*/
 typedef struct{
     TYPE* data;
     int rows, colums;
 }ns(Mat);
-/*================================== Utility Macros ==================================*/
+/*=================================================================================*/
+/*============================ Utility Macros ==================================*/
+/*=================================================================================*/
+// Check if number is inside de matrix
 #define isInside(Mat, i, j) ((i) > 0 && (i) <= (self->rows)) && ((j) > 0 && (j) <= (self->colums))?true:false
+
+// Check if data is NULL
 #define isNull(data) (data) == NULL? true: false
 
-/*===============================Error handling Macros ==================================*/
-#define ERROR(text) fprintf(stderr,"%s at file: %s\nfunction: %s \nline:  %d\n",text,__FILE__,__func__, __LINE__)
+// Converts two adress to linear 
+#define ADDRESS(self,i,j) ((i)*(self->colums)+(j))
 
+#define MGET(self,i,j) (self->data[((i)*(self->colums)+(j))])
+
+// Checks if the size of the two matrix is equal
+#define isSizeEqual(a,b) (a->rows==b->rows&&a->colums==b->colums)
+
+/*=================================================================================*/
+/*============================ Error handling Macros ==================================*/
+/*=================================================================================*/
+
+// Simple interface for error throwing
+#define ERROR(text) {\
+    fprintf(stderr,"%s at file: %s\nfunction: %s \nline:  %d\n",text,\
+                                                                __FILE__,\
+                                                                __func__,\
+                                                                __LINE__);\
+    assert(false);\                                          
+}\
+// Error in alocation fo memory
 #define ALLOC_ERROR ERROR("Memory did not allocate")
-#define CHECK_ALLOC(data) if(isNull((data))){ALLOC_ERROR;assert(false);}
+#define CHECK_ALLOC(data) if(isNull((data))){ALLOC_ERROR;}
 
+// Error receiving null argument in function 
 #define NULLARG_ERROR ERROR("Null arg received")
-#define CHECK_NULL(data) if(isNull((data))){NULLARG_ERROR;assert(false);}
+#define CHECK_NULL(data) if(isNull((data))){NULLARG_ERROR;}
 
+// Error trying to look at data out of the boundary of matrix
+#define OUTOFBOUND_ERROR ERROR("Out of boundary request")
+#define CHECK_OOB(data, i, j) if(!isInside((self),(i),(j))){OUTOFBOUND_ERROR;}
+
+// Matrices given arent the same size
+#define DIFERENTSIZE_ERROR ERROR("Matrix are of diferent size")
+#define CHECK_SIZE(a, b) if(!isSizeEqual(a,b)){DIFERENTSIZE_ERROR;assert(false);}
+/*=================================================================================*/
 /*================================== Definitions ==================================*/
+/*=================================================================================*/
 
 //  Create Matrix with i rows and j columns
 ns(Mat)* ns(Mat_create)(int i, int j);
@@ -52,16 +93,45 @@ ns(Mat)* ns(Mat_create)(int i, int j);
 //  Free Matrix from memory
 void     ns(Mat_destroy)(ns(Mat)* self);
 
-//  Get number in [i][j] position in matrix
-TYPE     ns(Mat_get)(ns(Mat)* self, int i, int j);
-
 //  Fill matrix with the x value
 ns(Mat)* ns(Mat_fill)(ns(Mat)* self,TYPE x);
 
 // Print matrix
-void     ns(Mat_print(ns(Mat)* self));
+void     ns(Mat_print)(ns(Mat)* self);
 
-/*================================== Matrix ==================================*/
+//  Get number in [i][j] position in matrix
+TYPE     ns(Mat_get)(ns(Mat)* self, int i, int j);
+
+// Set number in [i][j] position in matrix to value
+ns(Mat)* ns(Mat_set)(ns(Mat)* self, int i, int j, TYPE value);
+
+/*================================== Operations ==================================*/
+ns(Mat)* ns(Mat_add)(ns(Mat)* self, ns(Mat)* other){
+    CHECK_NULL(self);CHECK_NULL(other);
+    CHECK_SIZE(self,other);
+    int i, j;
+    for(i=0;i<self->rows;i++){
+        for(j=0;j<self->colums;j++){
+            MGET(self,i,j) += MGET(other,i,j);
+        }
+    }
+    return self;
+}
+ns(Mat)* ns(Mat_addC)(ns(Mat)* a ,ns(Mat)* b){
+    CHECK_NULL(a);CHECK_NULL(b);
+    CHECK_SIZE(a,b);
+    Mat *c = Mat_create(a->rows, a->colums);
+    CHECK_ALLOC(c);
+    int i, j;
+    for(i=0;i<c->rows;i++){
+        for(j=0;j<c->colums;j++){
+            MGET(c,i,j) = MGET(a,i,j) + MGET(b,i,j);
+        }
+    }
+}
+/*=================================================================================*/
+/*================================== Implementation ===============================*/
+/*=================================================================================*/
 ns(Mat)* ns(Mat_create)(int rows, int colums){
     ns(Mat)* matrix = malloc(sizeof(ns(Mat)*));
     CHECK_ALLOC(matrix);
@@ -86,22 +156,27 @@ void ns(Mat_destroy)(ns(Mat)* self){
     free(self->data);
     free(self);
 }
-TYPE ns(Mat_get)(ns(Mat)* self, int i, int j){
-    if(!isInside(self, i, j)){
-        printf("Out of bound");
-    }
-    return self->data[i*self->colums + j];
-}
 void ns(Mat_print(ns(Mat)* self)){
     CHECK_NULL(self);
     int i, j;
     printf("Matrix [%d][%d]\n", self->rows, self->colums); 
     for(i=0;i<self->rows;i++){
         for(j=0;j<self->colums;j++){
-            printf("[%d][%d]: %.3f ",i, j, self->data[i*self->colums + j]);
+            printf("[%d][%d]: %.3f ",i, j, MGET(self,i,j));
             printf("\n");
         }
     }
+}
+TYPE ns(Mat_get)(ns(Mat)* self, int i, int j){
+    CHECK_NULL(self);
+    CHECK_OOB(self, i ,j);
+    return MGET(self,i,j);
+}
+ns(Mat)* ns(Mat_set)(ns(Mat)* self, int i, int j, TYPE value){
+    CHECK_NULL(self);
+    CHECK_OOB(self, i ,j);
+    MGET(self,i,j) = value;
+    return self;
 }
 /*================================== Vec ==================================*/
 void ns(teste)(TYPE a){
